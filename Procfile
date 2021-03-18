b7d683cfdee3b81059f3d2af2b08f44f77ddd3b0
delattre1 <daniieeldelattre@gmail.com>
2021-03-18 15:37:45 -0300

    heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn getit.wsgi --log-file -
--- getit/settings.py
+++ getit/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'hj9c^)_ju(f1)(1_j-9a!=z8uy**2*xx77p&^m9q#is-n=!yd8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
