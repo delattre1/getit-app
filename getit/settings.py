f17c38b0395e75a443f6a13a05d6ca274dfd798c
delattre1 <daniieeldelattre@gmail.com>
2021-03-23 12:05:04 -0300

    flask config

--- getit/settings.py
+++ getit/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hj9c^)_ju(f1)(1_j-9a!=z8uy**2*xx77p&^m9q#is-n=!yd8'
+
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
